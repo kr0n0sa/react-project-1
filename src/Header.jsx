b19ebdaef6bc94f90a588308e710572f0930b7d7
kr0n0sa <gogibadani@gmail.com>
2026-03-10 18:52:07 +0400

    Updated Header and styles

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <h1>Word Guesser</h1>
       <p>
         guess the word withing 8 tries and i will consider buying you a shawarma
